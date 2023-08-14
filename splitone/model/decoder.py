cda42bd48fb0358bdb65fcd720ef1d7704196c2d
Xu Mingrui <255718431+marived@users.noreply.github.com>
2023-08-14 17:03:02 +0800

    scratch: mirror decoder

---
+++ splitone/model/decoder.py
@@ -0,0 +1,21 @@
+"""Decoder — mirror of the encoder, transposed convs."""
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+    def __init__(self, channels=(256, 128, 64, 32), strides=(8, 5, 4, 2)):
+        super().__init__()
+        layers = []
+        c_prev = channels[0]
+        for c, s in zip(channels[1:], strides[:-1]):
+            layers.append(nn.ConvTranspose1d(c_prev, c, kernel_size=2 * s,
+                                             stride=s, padding=s // 2))
+            layers.append(nn.GELU())
+            c_prev = c
+        layers.append(nn.ConvTranspose1d(c_prev, 1, kernel_size=2 * strides[-1],
+                                         stride=strides[-1],
+                                         padding=strides[-1] // 2))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, z):
+        return self.net(z)  # (B, 1, T)
