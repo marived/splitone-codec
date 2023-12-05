8c8ca4a0fe33127a2bb1e8db38bfc3d122cc0ba4
Xu Mingrui <255718431+marived@users.noreply.github.com>
2023-12-05 23:14:58 +0800

    loss: add multi-scale stft magnitude loss

--- splitone/losses.py
+++ splitone/losses.py
@@ -1,5 +1,28 @@
+"""Reconstruction losses."""
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+
+
+class MultiScaleSTFTLoss(nn.Module):
+    def __init__(self, n_ffts=(2048, 1024, 512, 256, 128), eps=1e-5):
+        super().__init__()
+        self.n_ffts = n_ffts
+        self.eps = eps
+
+    def _stft_mag(self, x, n_fft):
+        window = torch.hann_window(n_fft, device=x.device)
+        spec = torch.stft(x.squeeze(1), n_fft=n_fft, hop_length=n_fft // 4,
+                          win_length=n_fft, window=window, return_complex=True)
+        return spec.abs()
+
+    def forward(self, recon, target):
+        loss = 0.0
+        for n_fft in self.n_ffts:
+            r = self._stft_mag(recon, n_fft)
+            t = self._stft_mag(target, n_fft)
+            loss = loss + F.l1_loss(r, t)
+        return loss / len(self.n_ffts)
 
 
 def waveform_l1(recon, target):
