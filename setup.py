45aa5bb1e014a2889efbb8dd8d509f89ca7a9faa
Xu Mingrui <255718431+marived@users.noreply.github.com>
2024-02-18 01:01:09 +0800

    setup: name + version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="splitone-codec",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.10",
 )
